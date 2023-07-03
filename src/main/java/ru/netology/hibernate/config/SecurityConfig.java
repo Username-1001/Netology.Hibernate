a1a522c2628c7ad9b86145afa0ea3e7b813c4b40
Ravermeister <bgygblbketbl@mail.ru>
2023-07-04 02:27:56 +0300

    HW method-security

--- src/main/java/ru/netology/hibernate/config/SecurityConfig.java
+++ src/main/java/ru/netology/hibernate/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package ru.netology.hibernate.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -11,13 +12,15 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/persons/by-city").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .logout((logout) -> logout.permitAll());
@@ -26,11 +29,28 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user = User
-                .withUsername("user")
-                .password("{noop}password")
-                .build();
+        List<UserDetails> users = List.of(
+                User
+                        .withUsername("user_read")
+                        .password("{noop}password")
+                        .roles("READ")
+                        .build(),
+                User
+                        .withUsername("user_write")
+                        .password("{noop}password")
+                        .roles("WRITE")
+                        .build(),
+                User
+                        .withUsername("user_delete")
+                        .password("{noop}password")
+                        .roles("DELETE")
+                        .build(),
+                User
+                        .withUsername("Vasya")
+                        .password("{noop}password")
+                        .roles("READ")
+                        .build());
 
-        return new InMemoryUserDetailsManager(user);
+        return new InMemoryUserDetailsManager(users);
     }
 }
--- src/main/java/ru/netology/hibernate/controller/PersonController.java
+++ src/main/java/ru/netology/hibernate/controller/PersonController.java
@@ -1,9 +1,15 @@
 package ru.netology.hibernate.controller;
 
+import jakarta.annotation.security.RolesAllowed;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.netology.hibernate.entity.Person;
+import ru.netology.hibernate.entity.PersonId;
 import ru.netology.hibernate.repository.PersonRepository;
 
 import java.util.List;
@@ -18,17 +24,30 @@ public class PersonController {
     }
 
     @GetMapping("/persons/by-city")
+    @Secured("READ")
     public List<Person> getByCity(@RequestParam("city") String city) {
         return repository.findByCity(city);
     }
 
     @GetMapping("/persons/by-age")
+    @RolesAllowed("WRITE")
     public List<Person> getByAge(@RequestParam("age") int age) {
         return repository.findYoungerThan(age);
     }
 
     @GetMapping("/persons/by-name-surname")
+    @PostAuthorize("returnObject.get().id.name == authentication.principal.username")
     public Optional<Person> getByNameAndSurname(@RequestParam("name") String name, @RequestParam("surname") String surname) {
         return repository.findByFullname(name, surname);
+    }
+
+    @PostMapping("/persons/delete")
+    @PreAuthorize("hasAnyRole('WRITE', 'DELETE')")
+    public void deleteById(@RequestParam("name") String name, @RequestParam("surname") String surname, @RequestParam("age") int age) {
+        PersonId id = new PersonId();
+        id.setName(name);
+        id.setSurname(surname);
+        id.setAge(age);
+        repository.deleteById(id);
     }
 }
